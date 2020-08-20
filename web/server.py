ba8b2ead2981bfdc8e1e0f1626ec7aecde769abe
Patrick5455 <patrickojunde@gmail.com>
2020-08-20 06:03:28 +0100

    Add views for analysis and prediction

--- web/server.py
+++ web/server.py
@@ -1,28 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
 
+# define function to load model
+
+def load_model(path="web/models/", model_name=''):
+
+    pickle_in = open(path + model_name, "rb")
+    model = pickle.load(pickle_in)
+
+    return model
+
+
 @app.route("/", methods=['GET'])
 def home():
-    model = "Serve Model"
-    return render_template("home.html", model=model)
+
+    return render_template("index.html")
 
 
 @app.route("/about_us", methods=['GET'])
-def home():
+def about():
     model = "Serve Model"
     return render_template("about.html", model=model)
 
 
 @app.route("/predict", methods=['POST'])
 def prediction():
-    return render_template("predictions.html")
+
+    # to:do add logic to determine which model to load based the type pf prediction
+
+    model = load_model()
+    return render_template("prediction.html", model=model)
 
 
-@app.route("/visualize", methods=['POST'])
-def visualize():
-    return render_template("visualize.html")
+@app.route("/analysis", methods=['GET'])
+def analysis():
+    return render_template("analysis.html")
 
 
 if __name__ == '__main__':
