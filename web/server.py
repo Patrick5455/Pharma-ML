410cafdfa70903e6576b1595207b615a0984df5f
Patrick5455 <patrickojunde@gmail.com>
2020-08-20 05:28:01 +0100

    Add views for predictin and visualize

--- web/server.py
+++ web/server.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'])
 def home():
-    return
+    model = "Serve Model"
+    return render_template("home.html", model=model)
+
+
+@app.route("/predict", methods=['POST'])
+def prediction():
+    return render_template("predictions.html")
+
+
+@app.route("/visualize", methods=['POST'])
+def visualize():
+    return render_template("visualize.html")
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3333, debug=True)
+    app.run(host='localhost', port=3030, debug=True)
